962d18b18f2e831f42030c779392d05a616c7863
Brandon Russell <brussell98@users.noreply.github.com>
2024-08-23 16:14:56 -0500

    Prettier

--- inject.js
+++ inject.js
@@ -1,6 +1,6 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.method == "getToken") {
-        const token = localStorage.getItem('sp_access_token');
-        sendResponse({token: token});
-    }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.method == "getToken") {
+    const token = localStorage.getItem("sp_access_token");
+    sendResponse({ token: token });
+  }
 });
